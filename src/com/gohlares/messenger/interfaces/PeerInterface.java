f31bb062ea5a9ea7291f3a5257d788482247b6af
Rodrigo Tavares <rodrigost23@gmail.com>
2016-08-20 17:54:06 -0400

    Atualizando PeerInterface

--- src/com/gohlares/messenger/interfaces/PeerInterface.java
+++ src/com/gohlares/messenger/interfaces/PeerInterface.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 public interface PeerInterface extends Remote {
     public String getUUID() throws RemoteException;
     public PeerInfoInterface getInfo() throws RemoteException;
-    public boolean send(MessageInterface msg) throws RemoteException;
+    public boolean send(PeerInfoInterface from, MessageInterface msg) throws RemoteException;
     public boolean isAlive() throws RemoteException;
 }
